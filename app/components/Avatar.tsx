78105487b576c5b2ef7b6d2fe24f6be62667735a
yyuu0223kdev <blackrock.dev011@gmail.com>
2025-05-24 13:10:55 -0400

    google-github

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,14 +2,20 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+    src: string | null | undefined;
+}
+
+const  Avatar :React.FC<AvatarProps> = ({
+    src
+}) => {
     return (
         <Image
             alt="Avatar"
             className="rounded-full"
             height="30"
             width="30"
-            src="/images/placeholder.png"
+            src={src || "/images/placeholder.png"}
         />
     )
 }
--- app/components/navbar/UseMenu.tsx
+++ app/components/navbar/UseMenu.tsx
@@ -6,11 +6,11 @@ import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface userMeneProps {
-    currentUser?: User | null
+    currentUser?: SafeUser | null
 }
 
 const UseMenu: React.FC<userMeneProps> = ({
@@ -64,7 +64,7 @@ const UseMenu: React.FC<userMeneProps> = ({
                 >
                     <AiOutlineMenu />
                     <div className="hidden md:block">
-                        <Avatar />
+                        <Avatar src={currentUser?.image}/>
                     </div>
                 </div>
             </div>
